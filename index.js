4c82ac1b8bfbb82a651be769bf8321bcc8648028
Chad Boyce <chadboyce@gmail.com>
2022-10-07 02:33:56 +0000

    update

---
+++ index.js
@@ -0,0 +1,21 @@
+const axios = require('axios')
+
+
+
+
+// function to handle success
+function success() {
+    var data = JSON.parse(this.responseText); //parse the string to JSON
+    console.log(data);
+}
+
+// function to handle error
+function error(err) {
+    console.log('Request Failed', err); //error details will be in the "err" object
+}
+
+var xhr = new XMLHttpRequest(); //invoke a new instance of the XMLHttpRequest
+xhr.onload = success; // call success function if request is successful
+xhr.onerror = error;  // call error function if request failed
+xhr.open('GET', 'https://api.github.com/users/manishmshiva'); // open a GET request
+xhr.send(); // send the request to the server
