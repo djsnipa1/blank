30207ca2e9dc7b7da2d36b71a800af9451aefd71
Chad Boyce <chadboyce@gmail.com>
2024-02-15 16:19:47 +0000

    svelte circle animate

--- new/src/lib/components/Circle.svelte
+++ new/src/lib/components/Circle.svelte
@@ -1,3 +1,42 @@
 <script>
-	import { scale, fade } from 'svelte/transition';
+import { scale, fade } from 'svelte/transition';
+import { elasticOut } from 'svelte/easing';
+
+  let scaling = false;
+
+  function customScale() {
+    return {
+      duration:  10000,
+      easing: elasticOut,
+      css: (t, u) => `transform: scale(${t * 3000}%)` //`translateX(${u *  100}%)`
+    };
+  }
 </script>
+
+
+<div class="background min-w-full min-h-full flex justify-center items-center">
+<button on:click={() => { scaling = !scaling }} class="btn">Scale Bitch</button>
+  <!-- Your component content goes here -->
+  {#if scaling}
+  <div 
+    class="absolute border-white border-4 rounded-[100%] h-20 w-20 mx-auto"
+    transition:customScale
+    ></div>
+  {/if}
+</div>
+
+<style>
+  .background {
+    position: absolute;
+    height:  100%;
+    width:  100%;
+    top:  0;
+    left:  0;
+    background-color: #282828; 
+    /* background-color: #d2ff57; */
+    background-image: url("https://www.transparenttextures.com/patterns/3px-tile.png");
+    background-repeat: repeat;
+    background-size: auto;
+    z-index: -1; /* Places the background behind the content */
+  }
+</style>
--- new/src/routes/circles/+page.svelte
+++ new/src/routes/circles/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import { scale, fade } from 'svelte/transition';
-	import Circle from '$app/components/Circle.svelte';
+	//import Circle from '$lib/components/Circle.svelte';
+	import Circle from '$components/Circle.svelte';
 </script>
 
 <h1>Circles</h1>
+
 <Circle />
